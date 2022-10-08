66134a006014ae9ed7b74b6a79e13cf3c7b3e969
Vladimir Makarov <makarov.vladimir@phystech.edu>
2022-10-08 19:38:52 +0300

    Add multi-thread versions

--- benchmark.py
+++ benchmark.py
@@ -3,31 +3,34 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import subprocess
 import sys
-from time import sleep
 
 
-MIN_SIZE=100
-MAX_SIZE=150
-STEP=10
-NUM_OF_TESTS=10
+MIN_SIZE=128
+MAX_SIZE=128 * 16 + 1
+STEP_SIZE=128
+
+MIN_THREADS = 2
+MAX_THREADS = 17
+STEP_THREADS = 1
+
+NUM_OF_TESTS = 5
 
 TEMP_TEST_FILE_NAME = 'test.tmp'
 BIN_FILENAME = 'matrix_mul'
+PICTURE_SERIES = 'graph'
 
 def compile_file(file, outfile):
-    result = subprocess.run(["g++", "-std=c++11", "-o", outfile, file],\
+    result = subprocess.run(["g++", "-std=c++11", "-O4", "-o", outfile, file],\
         stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     if result.returncode != 0:
         raise Exception(f"can't compile {file}")
 def compile_input_file():
     if len(sys.argv) == 1:
         raise Exception("There are no programs to test")
-    if len(sys.argv) > 2:
-        raise Exception("Too many arguments")
     file = sys.argv[1]
     compile_file(file, BIN_FILENAME)
-def execute_program(file, mode = '1'):
-    return int(subprocess.check_output([f"./{file}", TEMP_TEST_FILE_NAME, mode]))
+def execute_program(file, mode = '1', thread_num = 16):
+    return int(subprocess.check_output([f"./{file}", TEMP_TEST_FILE_NAME, mode, str(thread_num)]))
 def remove_file(file):
     Path(file).unlink(missing_ok=True)
 def create_file(file):
@@ -38,10 +41,16 @@ def generate_test(size):
     matrices = np.random.randint(-100, 100, size=(size * 2, size))
     np.savetxt(TEMP_TEST_FILE_NAME, matrices, fmt="%d", header=str(size), comments="")
 
-sizes = np.arange(MIN_SIZE, MAX_SIZE, STEP)
+if len(sys.argv) < 2:
+    raise Exception("No picture name")
+if len(sys.argv) > 2:
+    raise Exception("Too many arguments")
+
+compile_input_file()
+
+sizes = np.arange(MIN_SIZE, MAX_SIZE, STEP_SIZE)
 times1 = []
 times2 = []
-compile_input_file()
 for size in sizes:
     print(size)
     average1 = np.array([])
@@ -52,12 +61,41 @@ for size in sizes:
         average2 = np.append(average2, execute_program(BIN_FILENAME, '2'))
     times1 = np.append(times1, np.average(average1))
     times2 = np.append(times2, np.average(average2))
-remove_file(BIN_FILENAME)
-remove_file(TEMP_TEST_FILE_NAME)
 
-plt.title('Наивная реализация')
-plt.plot(sizes, times1, color='red')
-plt.plot(sizes, times2, color='blue')
+plt.figure('single-thread')
+plt.title('Однопоточные реализации')
+plt.plot(sizes, times1, color='red', label='naive')
+plt.plot(sizes, times2, color='blue', label='blocky')
 plt.xlabel('Размер матрицы')
 plt.ylabel('Время, мсек')
-plt.savefig('attempt3.png')
+plt.legend()
+plt.savefig(f'{PICTURE_SERIES}1.png')
+
+
+# size = int(np.sqrt(5 * (2 ** 20)))
+size = 1000
+threads = np.arange(MIN_THREADS, MAX_THREADS, STEP_THREADS)
+times3 = []
+times4 = []
+for num_of_threads in threads:
+    print(num_of_threads)
+    average3 = np.array([])
+    average4 = np.array([])
+    for _ in np.arange(0, NUM_OF_TESTS):
+        generate_test(size)
+        average3 = np.append(average3, execute_program(BIN_FILENAME, '3', num_of_threads))
+        average4 = np.append(average4, execute_program(BIN_FILENAME, '4', num_of_threads))
+    times3 = np.append(times3, np.average(average3))
+    times4 = np.append(times4, np.average(average4))
+
+plt.figure('multi-thread')
+plt.title('Многопоточные реализации')
+plt.plot(threads, times3, color='red', label='naive')
+plt.plot(threads, times4, color='blue', label='block')
+plt.xlabel('Количество потоков')
+plt.ylabel('Время, мсек')
+plt.legend()
+plt.savefig(f'{PICTURE_SERIES}2.png')
+
+remove_file(BIN_FILENAME)
+remove_file(TEMP_TEST_FILE_NAME)
--- matrix.cpp
+++ matrix.cpp
@@ -1,51 +1,79 @@
-#include <assert.h>
+#include <cassert>
 #include <chrono>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <fstream>
+#include <thread>
+#include <vector>
 
-#include <stdlib.h>
 
 namespace {
-  const size_t BLOCK_SIZE = 16; 
+  constexpr size_t BLOCK_SIZE = 16;
 }
 
 class MatrixMultiplication{
   public:
-    MatrixMultiplication(const std::string& filename, char mode = '1');
+    MatrixMultiplication(const std::string& filename, char mode, size_t num_of_threads);
     int Run();
+    ~MatrixMultiplication();
   private:
     const char mode_;
-    int* left_;
-    int* right_;
-    int* result_;
+    int* left_ = nullptr;
+    int* right_ = nullptr;
+    int* result_ = nullptr;
     size_t size_;
     size_t capacity_;
+    size_t num_of_threads_;
 
-    int& at(int* arr, size_t row, size_t col) { return arr[row * capacity_ + col]; }
-    int& left(size_t row, size_t col) { return at(left_, row, col); }
-    int& right(size_t row, size_t col) { return at(right_, row, col); }
-    int& result(size_t row, size_t col) { return at(result_, row, col); }
+    inline int& at(int* arr, size_t row, size_t col) { return arr[row * capacity_ + col]; }
+    inline int& left(size_t row, size_t col) { return at(left_, row, col); }
+    inline int& right(size_t row, size_t col) { return at(right_, row, col); }
+    inline int& result(size_t row, size_t col) { return at(result_, row, col); }
+    
+    int* allocate_2d_array();
 
     void ReadArrayFrom(std::ifstream& f_in, int*& arr);
     void ReadFile(const std::string& filename);
     void NaiveSingleThread();
     void BlockSingleThread();
+    void NaiveMultiThread();
+    void BlockMultiThread();
 };
 
-MatrixMultiplication::MatrixMultiplication(const std::string& filename, char mode)
-    : mode_(mode) {
-  if (mode < '1' || mode > '4') {
-    throw std::invalid_argument("mode should be from 1 to 4");
+MatrixMultiplication::MatrixMultiplication(const std::string& filename, char mode, size_t num_of_threads)
+    : mode_(mode), num_of_threads_(num_of_threads) {
+  if (mode_ < '1' || mode_ > '4') {
+    throw std::invalid_argument("mode should be from 1 to 4" + mode_);
   }
   ReadFile(filename);
-  result_ = reinterpret_cast<int*>(std::malloc(sizeof(int) * capacity_ * size_));
-  for (int i = 0; i < capacity_ * size_; ++i) {
-    result_[i] = 0;
+  result_ = allocate_2d_array();
+}
+
+MatrixMultiplication::~MatrixMultiplication() {
+      if (left_) {
+        std::free(left_);
+      }
+      if (right_) {
+        std::free(right_);
+      }
+      if (result_) {
+        std::free(result_);
+      }
+    }
+
+int* MatrixMultiplication::allocate_2d_array() {
+  void* alloc = std::malloc(sizeof(int) * capacity_ * capacity_);
+  int* arr = reinterpret_cast<int*>(alloc);
+  for (int i = 0; i < capacity_ * capacity_; ++i) {
+    arr[i] = 0;
   }
+  return arr;
 }
 
 void MatrixMultiplication::ReadArrayFrom(std::ifstream& f_in, int*& arr) {
-  arr = reinterpret_cast<int*>(sizeof(int) * capacity_ * size_);
+  // malloc просто для выравнивания
+  arr = allocate_2d_array();
   for (size_t i = 0; i < size_; ++i) {
     for (size_t j = 0; j < size_; ++j)
       f_in >> at(arr, i, j);
@@ -55,7 +83,7 @@ void MatrixMultiplication::ReadArrayFrom(std::ifstream& f_in, int*& arr) {
 void MatrixMultiplication::ReadFile(const std::string& filename) {
   std::ifstream f_in(filename);
   if (!f_in.is_open()) {
-    throw "failed to open " + filename;
+    throw std::invalid_argument("failed to open " + filename);
   }
   f_in >> size_;
   capacity_ = (size_ / 64 + static_cast<bool>(size_ & 63)) * 64;
@@ -74,6 +102,12 @@ int MatrixMultiplication::Run() {
   case '2':
     BlockSingleThread();
     break;
+  case '3':
+    NaiveMultiThread();
+    break;
+  case '4':
+    BlockMultiThread();
+    break;
   default:
     throw std::invalid_argument("mode should be from 1 to 4");
     break;
@@ -91,23 +125,15 @@ void MatrixMultiplication::NaiveSingleThread() {
     }
   }
 }
-// static int counter = 0;
+
 void MatrixMultiplication::BlockSingleThread() {
-  const size_t full_blocks = size_ / BLOCK_SIZE; 
-  const size_t num_of_blocks = full_blocks + static_cast<bool>(size_ % BLOCK_SIZE);
-  for (size_t row_1 = 0; row_1 < size_; row_1 += BLOCK_SIZE) {
-    for (size_t col_1 = 0; col_1 < size_; col_1 += BLOCK_SIZE) {
-      for (size_t row_2 = 0; row_2 < size_; row_2 += BLOCK_SIZE) {
-        for (size_t col_2 = 0; col_2 < size_; col_2 += BLOCK_SIZE) {
-          size_t row_max = std::min(row_1 + BLOCK_SIZE, size_);
-          size_t col_max = std::min(col_2 + BLOCK_SIZE, size_);
-          size_t k_max = std::min(std::min(BLOCK_SIZE, size_ - col_1),
-            size_ - row_2);
-          for (size_t row = row_1; row < row_max; ++row) {
-            for (size_t k = 0; k < k_max; ++k) {
-              for (size_t col = col_2; col < col_max; ++col) {
-                result(row, col) += left(row, k + col_1) * right(k + row_2, col);
-              }
+  for (size_t b_row = 0; b_row < size_; b_row += BLOCK_SIZE) {
+    for (size_t b_k = 0; b_k < size_; b_k += BLOCK_SIZE) {
+      for (size_t b_col = 0; b_col < size_; b_col += BLOCK_SIZE) {
+        for (size_t add_row = 0; add_row < BLOCK_SIZE; ++add_row) {
+          for (size_t add_k = 0; add_k < BLOCK_SIZE; ++add_k) {
+            for (size_t add_col = 0; add_col < BLOCK_SIZE; ++add_col) {
+              result(b_row + add_row, b_col + add_col) += left(b_row + add_row, b_k + add_k) * right(b_k + add_k, b_col + add_col);
             }
           }
         }
@@ -116,10 +142,62 @@ void MatrixMultiplication::BlockSingleThread() {
   }
 }
 
+void MatrixMultiplication::NaiveMultiThread() {
+  std::vector<std::thread> threads;
+  threads.reserve(num_of_threads_);
+  for (size_t thread_num = 0; thread_num < num_of_threads_; ++thread_num) {
+    threads.emplace_back([this, thread_num] () {
+      for (size_t i = thread_num; i < size_; i += num_of_threads_) {
+        for (size_t j = 0; j < size_; ++j) {
+          for (size_t k = 0; k < size_; ++k) {
+            result(i, j) += left(i, k) * right(k, j);
+          }
+        }
+      }
+    });
+  }
+  for (auto& thread : threads) {
+    thread.join();
+  }
+}
+
+void MatrixMultiplication::BlockMultiThread() {
+  std::vector<std::thread> threads;
+  threads.reserve(num_of_threads_);
+  size_t blocks_in_row = capacity_ / BLOCK_SIZE;
+  size_t num_of_blocks = blocks_in_row * blocks_in_row;
+  for (size_t thread_num = 0; thread_num < num_of_threads_; ++thread_num) {
+    threads.emplace_back([this, thread_num, num_of_blocks, blocks_in_row]() {
+      for (size_t block_num = thread_num; block_num < num_of_blocks; block_num += num_of_threads_) {
+        size_t block_row = block_num / blocks_in_row;
+        size_t block_col = block_num % blocks_in_row;
+        size_t row_begin = block_row * BLOCK_SIZE;
+        size_t col_begin = block_col * BLOCK_SIZE;
+        size_t row_end = row_begin + BLOCK_SIZE;
+        size_t col_end = row_end + BLOCK_SIZE;
+        for (size_t row = row_begin; row < row_end; ++row) {
+          for (size_t k = 0; k < size_; ++k) {
+            for (size_t col = col_begin; col < col_end; ++col) {
+              result(row, col) += left(row, k) * right(k, col);
+            }
+          }
+        }
+        
+      }
+    });
+  }
+  
+  for (auto& thread : threads) {
+    thread.join();
+  }
+}
+
 int main(int argc, char** argv) {
-  assert(argc == 2 || argc == 3);
-  MatrixMultiplication mul{std::string(argv[1]), (argc == 3 ? argv[2][0] : '1')};
+  assert(argc >= 2 && argc <= 4);
+  std::string file{argv[1]};
+  char mode = (argc >= 3 ? argv[2][0] : '1');
+  size_t num_of_threads = (argc >= 4 ? atoi(argv[3]) : 16);
+  MatrixMultiplication mul{file, mode, num_of_threads};
   std::cout << mul.Run();
-  // std::cout << std::endl << counter << std::endl;
   return 0;
 }
